14af6e54ed89a639db4b049327cdaa68d3f1f935
rahul499 <rahul4mishra99@gmail.com>
2020-05-14 22:11:56 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -26,9 +26,42 @@ background-repeat: no-repeat;
 background-size: cover;
 border:5px solid black;
 }
-</style>
 
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+.tablee{
+  border: 4px #2b2b2b solid;
+width: 80%;
+max-width: 2500px;
+min-width: 500px;
+ border-style:solid; 
+ border-collapse:collapse;
+}
+td, th {
+border: 4px #2b2b2b solid;
+padding:12px;
+}
+
+.text-dark{
+  background-color: #fde9d9;
+}
+
+#submit {
+ background-color: #bbb;
+ padding: .5em;
+ -moz-border-radius: 5px;
+ -webkit-border-radius: 5px;
+ border-radius: 6px;
+ color: #fff;
+ font-family: 'Oswald';
+ font-size: 20px;
+ text-decoration: none;
+ border: none;
+}
+#submit:hover {
+ border: none;
+ background: orange;
+ box-shadow: 0px 0px 1px #777;
+}
+</style>
 
 <link rel="stylesheet" href="css/index.css">
 
@@ -41,8 +74,8 @@ border:5px solid black;
 
 <div class="list">
 <ul><br>
- <center>  <a href="insertemployee.php" class="col-lg-3"><button class="btn btn-success col-lg-3">Add</button></a> 
-        <a href="logout.php" class="col-lg-3"><button class="btn btn-success col-lg-3" name="logout">Logout</button></a>
+ <center>  <a href="insertemployee.php" ><button id="submit" style=" width: 250px; margin-right: 80px;">Add</button></a> 
+        <a href="login.php" ><button id="submit" name="logout" style=" width: 250px; margin-right: 50px;">Logout</button></a>
   </center>
   <br>
 </ul>
@@ -54,23 +87,25 @@ border:5px solid black;
   <br><br>
   <div class="col-lg-12"><br>
     <div class="row">
-    <h2 class="col-lg-6">Displaying Records</h2>
+  <center><h2 class="col-lg-6" style="font-size: 40px;">Displaying Records</h2></center>  
         
     </div><br>
-    <table class="table table-stripped table-hover table-bordered" style="border: 10px;">
+    <center>
+    <table class ="tablee" >
       <tr class="text-dark">
-        <th><h5>ID</h5></th>
-        <th><h5>Name</h5></th>
-        <th><h5>Birth Date</h5></th>
-        <th><h5>Mobile</h5></th>
-        <th><h5>Address</h5></th>
-        <th><h5>Salary</h5></th>
-        <th><h5>Qualification</h5></th>
-        <th><h5>Job Profile</h5></th>
-        <th><h5>Date of Join</h5></th>
-        <th><h5>View</h5></th>
+      <b>  <th>ID</th>
+        <th>Name</th>
+        <th>Birth Date</th>
+        <th>Mobile</th>
+        <th>Email</th>
+        <th>Address</th>
+        <th>Salary</th>
+        <th>Qualification</th>
+        <th>Job Profile</th>
+        <th>Date of Join</th>
+        <th>View</th></b>
       </tr>
-
+   </center>
       
       <?php
 
@@ -78,16 +113,17 @@ while ($res = mysqli_fetch_array($query)) {
 ?>
 <center>
       <tr>
-        <th width="5%"><?php echo $res['emp_id'] ?></th>
+       <h5> <th width="5%"><?php echo $res['emp_id'] ?></th>
         <th width="14%"><?php echo $res['emp_name'] ?></th>
         <th width="16%" ><?php echo $res['emp_birthdate'] ?></th>
         <th><?php echo $res['emp_mobile'] ?></th>
+        <th width="16%"><?php echo $res['email'] ?></th>
         <th width="12%" ><?php echo $res['emp_address'] ?></th>
         <th><?php echo $res['emp_salary'] ?></th>
         <th><?php echo $res['emp_qualification'] ?></th>
         <th width="12%" ><?php echo $res['emp_job'] ?></th>
         <th width="15%" ><?php echo $res['emp_joindate'] ?></th>
-        <th><a  href="update.php?id=<?php echo $res['emp_id']; ?>" class="text-white"><button class="btn btn-success">View</button></a></th>
+        <th><a  href="update.php?id=<?php echo $res['emp_id']; ?>" class="text-white"><button id="submit">View</button></a></th></h5>
       </tr>
     </center>
 <?php }
